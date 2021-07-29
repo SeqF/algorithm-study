66bf9871fcddb7ba89fd9369823536fa022f3c5f
paksu.fang <Paksu.BS.Fang@cityline.com.hk>
2021-07-29 12:35:40 +0800

    leetcode 105

--- LeetCode/src/leetcode150/medium/LeetCode_105.java
+++ LeetCode/src/leetcode150/medium/LeetCode_105.java
@@ -11,7 +11,31 @@ import tree.base.TreeNode;
  */
 public class LeetCode_105 {
 
+    /**
+     * 前序遍历获取根节点，然后在中序遍历找到该根节点，对左右子树继续处理
+     * @param preorder
+     * @param inorder
+     * @return
+     */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return createTree(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+    }
 
+    private TreeNode createTree(int[] preorder, int[] inorder, int l1, int r1, int l2, int r2) {
+        if (l1 > r2) {
+            return null;
+        }
+        TreeNode root = new TreeNode();
+        root.value = preorder[l1];
+        int i;
+        for (i = l2; i < r2; i++) {
+            if (preorder[l1] == inorder[i]) {
+                break;
+            }
+        }
+        //获取左右子树在前、中序遍历中的范围
+        root.left = createTree(preorder, inorder, l1 + 1, l1 + i - l2, l2, i - 1);
+        root.right = createTree(preorder, inorder, l1 + i - l2 + 1, r1, i + 1, r2);
+        return root;
     }
 }
