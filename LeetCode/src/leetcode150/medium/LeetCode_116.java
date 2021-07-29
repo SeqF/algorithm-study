99d38ccbb377f71c26801902bd6af94701e8fca2
paksu.fang <Paksu.BS.Fang@cityline.com.hk>
2021-07-29 13:17:13 +0800

    update

---
+++ LeetCode/src/leetcode150/medium/LeetCode_116.java
@@ -0,0 +1,15 @@
+package leetcode150.medium;
+
+import linkedlist.base.ListNode;
+
+/**
+ * 填充每个节点的下一个右侧节点指针
+ * @author ps
+ * 链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
+ */
+public class LeetCode_116 {
+
+    public ListNode connect(ListNode root) {
+
+    }
+}
