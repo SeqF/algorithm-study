63fdbbfa19d4704dca4e88773b2cbf90d4c8d88c
paksu.fang <Paksu.BS.Fang@cityline.com.hk>
2021-07-30 11:25:56 +0800

    update

--- LeetCode/src/leetcode150/medium/LeetCode_116.java
+++ LeetCode/src/leetcode150/medium/LeetCode_116.java
@@ -2,14 +2,67 @@ package leetcode150.medium;
 
 import linkedlist.base.ListNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 填充每个节点的下一个右侧节点指针
+ *
  * @author ps
  * 链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
  */
 public class LeetCode_116 {
 
-    public ListNode connect(ListNode root) {
+    /**
+     * 层次遍历来来进行连接？
+     *
+     * @param root
+     * @return
+     */
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                if (i == size - 1) {
+                    node.next = null;
+                }else {
+                    node.next = queue.peek();
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return root;
+    }
 
+    private class Node {
+        private int val;
+        private Node left;
+        private Node right;
+        private Node next;
+
+        public Node() {
+        }
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node left, Node right, Node next) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+            this.next = next;
+        }
     }
 }
