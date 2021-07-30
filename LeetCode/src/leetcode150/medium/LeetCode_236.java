c2ed4b7cb467c0714a4278b356cc162d637c96e8
paksu.fang <Paksu.BS.Fang@cityline.com.hk>
2021-07-30 16:34:52 +0800

    update

---
+++ LeetCode/src/leetcode150/medium/LeetCode_236.java
@@ -0,0 +1,15 @@
+package leetcode150.medium;
+
+import tree.base.TreeNode;
+
+/**
+ * 二叉树的最近公共祖先
+ * @author paksu
+ * 链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ */
+public class LeetCode_236 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+    }
+}
