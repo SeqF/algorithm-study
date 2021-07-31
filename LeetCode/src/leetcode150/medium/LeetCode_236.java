7099a3b77e023c03508b6eb1964a6c506e5985d0
boss <505475064@qq.com>
2021-07-31 16:37:46 +0800

    leetcode236

--- LeetCode/src/leetcode150/medium/LeetCode_236.java
+++ LeetCode/src/leetcode150/medium/LeetCode_236.java
@@ -1,5 +1,6 @@
 package leetcode150.medium;
 
+
 import tree.base.TreeNode;
 
 /**
@@ -9,7 +10,42 @@ import tree.base.TreeNode;
  */
 public class LeetCode_236 {
 
+    /**
+     * 树的后序遍历，自底向上
+     * 在树的后续遍历基础上进行查询结果的向上传递
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return find(root, p, q);
+    }
 
+    private TreeNode find(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = find(root.left, p, q);
+        TreeNode right = find(root.right, p, q);
+//        if (left !=null && right !=null) {
+//            return root;
+//        } else if (left == null && right == q) {
+//            return right;
+//        } else if (right == null && left == p) {
+//            return left;
+//        }else {
+//            return null;
+//        }
+        //逻辑简化
+        if (left != null && right != null) {
+            //当left和right都不为空时，即找到目标节点，其左右子节点都符合条件，此时将root返回
+            return root;
+        }
+        if (left == null) {
+            //当左节点为空，右节点不为空，说明右子树找到了目标节点，此时将right返回;左子树同理
+            return right;
+        }
+        return left;
     }
 }
